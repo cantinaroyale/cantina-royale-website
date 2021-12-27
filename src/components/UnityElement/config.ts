36eec0a78d67521bb5809a3d2527ac37fb7b1cf9
Denisn <denis.nikitin@gotech.io>
2021-12-27 16:32:09 +0200

    a

--- src/components/UnityElement/config.ts
+++ src/components/UnityElement/config.ts
@@ -1,6 +1,6 @@
 const unityConfig = {
   dataUrl: "nft-viewer/Build/WebGL.data.gz",
-  frameworkUrl: "nft-viewer/Build/WebGL.framework.js.gz",
+  frameworkUrl: "nft-viewer/Build/WebGL.framework.js",
   codeUrl: "nft-viewer/Build/WebGL.wasm.gz",
   streamingAssetsUrl: "StreamingAssets",
   companyName: "Phenomenon Games",
